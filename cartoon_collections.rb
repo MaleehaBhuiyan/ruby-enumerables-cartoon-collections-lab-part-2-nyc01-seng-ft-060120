7d218ccb58cd3dab345a8db0697cedc8a8161419
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-19 22:59:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.each do |element|
     new_array << element.capitalize
   end
+  new_array
 end
 
 def long_planeteer_calls(planeteer_calls)
